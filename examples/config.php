22aa73fe438d831e57d7f8f29c10565c72c9f2df
mostafaKeen <mostafa@keenenter.com>
2026-03-09 16:29:27 +0200

    westgate placment 2

--- examples/config.php
+++ examples/config.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'webhook_url' => 'https://westgate.bitrix24.com/rest/9034/i0r11egmkpnww9em/',
-    'BITRIX24_PHP_SDK_APPLICATION_CLIENT_ID' => 'local.69aecae8e7cd90.22555231',
-    'BITRIX24_PHP_SDK_APPLICATION_CLIENT_SECRET' => 'ttdvoiSOtXs6CojVlDbUqR6wD981YsLTOnMvD3yVDL9Nm6qyIA',
+    'webhook_url' => 'https://westgate.bitrix24.com/rest/9034/nks666y4mpf9ppx6/',
+    'BITRIX24_PHP_SDK_APPLICATION_CLIENT_ID' => 'local.698d7ea4e21af1.47863318',
+    'BITRIX24_PHP_SDK_APPLICATION_CLIENT_SECRET' => 'mhv4CulaEL7M5Y2zI98k43SG3aCfXMFMezMHOU0HnKAEVXK0dr',
     'BITRIX24_PHP_SDK_APPLICATION_SCOPE' => 'imconnector,im,crm,placement',
     'whatsapp_api_token' => 'EAAMrqIJHlTwBQxGBiKH0HZAIiZAGKbMR6LZBdp2LBXSlmFbv2ZBtJ9VroDsaow0GmnHCdfM975VnNgW9mZAuMBRa4ZAunG3y7XG6LSSpggJoXQF0DGYryOoqr8XAXZAdtUqM8luyRhn6CeamU5uOi26wD67pAnTUQDkIfsuiCcEDm1Cqk33Xoo3YcW9FC6dvTIjZBDZBzCzBfUZCuoGPS7eZA0EDt723TBjzeSdiolBKGYTB3bftNZANksST7UI5Bj5njQTyjAtjEWLX6oRz7ilmMlpf',
     'gupshup_app_id' => 'fc5f9e33-0bce-46d4-8600-070182ad15e5',
