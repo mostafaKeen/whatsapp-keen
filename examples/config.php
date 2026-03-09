506be0531b158fd5713de510ff13def2a3089aba
mostafaKeen <mostafa@keenenter.com>
2026-03-09 15:32:44 +0200

    westgate ready

--- examples/config.php
+++ examples/config.php
@@ -1,13 +1,13 @@
 <?php
 return [
-    'webhook_url' => 'https://keenenter.bitrix24.com/rest/807/zr7yk1vc1883tdfh/',
-    'BITRIX24_PHP_SDK_APPLICATION_CLIENT_ID' => 'local.698d7ea4e21af1.47863318',
-    'BITRIX24_PHP_SDK_APPLICATION_CLIENT_SECRET' => 'mhv4CulaEL7M5Y2zI98k43SG3aCfXMFMezMHOU0HnKAEVXK0dr',
+    'webhook_url' => 'https://westgate.bitrix24.com/rest/9034/qhl2j2p7gdbsftn1/',
+    'BITRIX24_PHP_SDK_APPLICATION_CLIENT_ID' => 'local.69aecae8e7cd90.22555231',
+    'BITRIX24_PHP_SDK_APPLICATION_CLIENT_SECRET' => 'ttdvoiSOtXs6CojVlDbUqR6wD981YsLTOnMvD3yVDL9Nm6qyIA',
     'BITRIX24_PHP_SDK_APPLICATION_SCOPE' => 'imconnector,im,crm',
     'whatsapp_api_token' => 'EAAMrqIJHlTwBQxGBiKH0HZAIiZAGKbMR6LZBdp2LBXSlmFbv2ZBtJ9VroDsaow0GmnHCdfM975VnNgW9mZAuMBRa4ZAunG3y7XG6LSSpggJoXQF0DGYryOoqr8XAXZAdtUqM8luyRhn6CeamU5uOi26wD67pAnTUQDkIfsuiCcEDm1Cqk33Xoo3YcW9FC6dvTIjZBDZBzCzBfUZCuoGPS7eZA0EDt723TBjzeSdiolBKGYTB3bftNZANksST7UI5Bj5njQTyjAtjEWLX6oRz7ilmMlpf',
-    'gupshup_app_id' => '977174cf-1792-4cac-b8d0-88de34646d75',
-    'gupshup_api_token' => 'sk_f24369e4bf7147d1b23edaaf4f63d5d2',
-    'gupshup_source' => '15557967143', // Replace with your Gupshup source number
-    'gupshup_app_name' => 'KEEN26WA', // Replace with your Gupshup app name
+    'gupshup_app_id' => 'fc5f9e33-0bce-46d4-8600-070182ad15e5',
+    'gupshup_api_token' => 'sk_1cf8c3ae6c024e04b896dd8c51f5d247',
+    'gupshup_source' => '97143927677', // Replace with your Gupshup source number
+    'gupshup_app_name' => 'Westgatedubai', // Replace with your Gupshup app name
     'var_dir' => __DIR__ . '/../var', // Base data directory
 ];
