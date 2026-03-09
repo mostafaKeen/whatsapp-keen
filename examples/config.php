a6e2799f4e799e0ba8d09e1aa99b3513ff0a69f9
mostafaKeen <mostafa@keenenter.com>
2026-03-09 16:06:20 +0200

    westgate partener token

--- examples/config.php
+++ examples/config.php
@@ -6,7 +6,7 @@ return [
     'BITRIX24_PHP_SDK_APPLICATION_SCOPE' => 'imconnector,im,crm',
     'whatsapp_api_token' => 'EAAMrqIJHlTwBQxGBiKH0HZAIiZAGKbMR6LZBdp2LBXSlmFbv2ZBtJ9VroDsaow0GmnHCdfM975VnNgW9mZAuMBRa4ZAunG3y7XG6LSSpggJoXQF0DGYryOoqr8XAXZAdtUqM8luyRhn6CeamU5uOi26wD67pAnTUQDkIfsuiCcEDm1Cqk33Xoo3YcW9FC6dvTIjZBDZBzCzBfUZCuoGPS7eZA0EDt723TBjzeSdiolBKGYTB3bftNZANksST7UI5Bj5njQTyjAtjEWLX6oRz7ilmMlpf',
     'gupshup_app_id' => 'fc5f9e33-0bce-46d4-8600-070182ad15e5',
-    'gupshup_api_token' => 'sk_1cf8c3ae6c024e04b896dd8c51f5d247',
+    'gupshup_api_token' => 'sk_b0d50d909be34f1b9e6fe9e6c866bca9',
     'gupshup_source' => '97143927677', // Replace with your Gupshup source number
     'gupshup_app_name' => 'Westgatedubai', // Replace with your Gupshup app name
     'var_dir' => __DIR__ . '/../var', // Base data directory
