ecc7be0091e77f64589c2d36dd3f8dc01995a48f
mostafaKeen <mostafa@keenenter.com>
2026-03-11 11:38:46 +0200

    debug errors

--- examples/custom-whatsapp-app/install.php
+++ examples/custom-whatsapp-app/install.php
@@ -1,5 +1,10 @@
 <?php
 declare(strict_types=1);
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 // If Bitrix24 sends installation POST data, handle the placement binding immediately.
